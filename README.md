57cb7efb6585c63de1dad16a861e63d8be981b4a
Theo Daly <theo.daly@outlook.com>
2018-11-02 00:03:57 -0500

    fixing up the read me

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Hang-Man-Game
+## What is this repo project?
+
+* This is a word guess game like hangman created using Javascript. 
+
+## How does it work?
+
+* The user inputs a letter guess, if that letter is in the word it will show the letter, if not, the letter will appear in a wrong guess div and add a body to our "man".
+* The website relies on Javascript loops, buttons, and functions to power the game.
+
+## Who will use this repo?
+
+* This is for coursework utilizing Javascript, but is open to anyone who wants to explore the code or play with it for fun.
+
+## What is the goal of the project?
+
+* Generate a random word.
+* When the user guesses a letter, it either reveals the letter in the word or goes to the incorrect guesses.
+* Letters should hide until the user guesses them.
+* Alert the user if they win by correctly guessing the word, or lose by having too many incorrect guesses.
+* Keep a tally of wins and losses.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,99 @@
-var phrases = [
-    "BOP",
-    "CRISPY",
-    "I LIKE THAT ENERGY",
-];
 
-//choose a random word
-var chosenWord = phrases[1];
 
-//display one asterisk per letter
-//for a specific word
-//slect the element from the DOM
-//using the document
-var asterisksSpan = document.getElementById("asterisks");
-var asteriskWord = [];
-var individualLetters = chosenWord.split("");
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var options = ["sandals", "margharita", "ocean", "boardwalk", "coastal","sandbar", "barnacle","beachball","bikini","catamaran", "lagoon", "lifeguard", "frisbee","underwater", "paddleboat", "pelican", "umbrella", "popsicle", "sunscreen", "swimming", "sailboat", "sandcastle", "seashell", "seashore", "snorkel", "starfish", "sunbathe", "sunburn", "sunglasses"];
 
-console.log(individualLetters);
-
-for (var i = 0; i < chosenWord.length; i++) {
-    asteriskWord = asteriskWord + "*";
+function getRandomIndex(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-asterisksSpan.innerText = asteriskWord;
-console.log(asteriskWord);
+function getRandomWord() {
+    var index = getRandomIndex(0, options.length);
+    return options[index];
+}
 
-//take user input and compare to specific word
-document.onkeyup = function (event) {
-    var key = event.key.toUpperCase();
-
-    for (var i = 0; i<individualLetters.length; i++) {
-        if (key === individualLetters[i].toUpperCase()) {
-            asteriskWord[i] = individualLetters[i];
+function asteriskWord(randomWord, correct) {
+    var newString = "";
+    for (let i = 0; i < randomWord.length; i++) {
+        if (correct.includes(randomWord[i])) {
+            newString += randomWord[i];
+        } else {
+            newString += "*";
         }
     }
+    return newString;
 }
 
-console.log(asteriskWord);
+function reset() {
+    var word = getRandomWord()
+    return {
+        guesses: 0,
+        currentWord: word,
+        hiddenWord: asteriskWord(word, []),
+        correctLetters: [],
+        incorrectLetters: []
+    };
+}
 
-//display each letter guessed
-//keep score
-//update screen with the updated word
+function displayStats(gameState, wins, losses) {
+    document.getElementById("wins").innerText = wins;
+    document.getElementById("losses").innerText = losses;
+    document.getElementById("guessesLeft").innerText = guessLimit - gameState.guesses;
+    document.getElementById("incorrect").innerText = gameState.incorrectLetters;
+    document.getElementById("hidden").innerText = gameState.hiddenWord;
+    var url = "https://www.ego4u.com/images/games/hangman0" + (gameState.guesses + 1);
+    if (gameState.guesses === guessLimit)
+        url += ".gif";
+        else
+        url += ".png"
+    document.getElementById("pic").setAttribute("src" , url);
+}
+
+function hasWon(gameState) {
+    return !gameState.hiddenWord.includes("*");
+}
+
+function hasLost(gameState) {
+    return gameState.guesses === guessLimit;
+}
+
+window.onload = function () {
+    var wins = 0;
+    var losses = 0;
+
+    var gameState = reset();
+
+    displayStats(gameState, wins, losses);
+
+    document.onkeyup = function (event) {
+        var key = event.key.toUpperCase();
+        var isInWord = gameState.currentWord.includes(key);
+        if (!alphabet.includes(key))
+            return;
+        if (isInWord) {
+            if (!gameState.correctLetters.includes(key)) {
+
+                gameState.correctLetters.push(key);
+                var newHidden = asteriskWord(gameState.currentWord, gameState.correctLetters);
+                gameState.hiddenWord = newHidden;
+            }
+        } else {
+            if (!gameState.incorrectLetters.includes(key)) {
+                gameState.incorrectLetters.push(key);
+                gameState.guesses += 1;
+            }
+        }
+        if (hasWon(gameState)) {
+            wins++;
+            alert("YOU WON!!");
+            gameState = reset();
+        } else if (hasLost(gameState)) {
+            losses++;
+            setTimeout(function() {
+                alert("You lost");
+                gameState = reset();
+            }, 3000);
+        }
+        displayStats(gameState, wins, losses);
+    }
+}
